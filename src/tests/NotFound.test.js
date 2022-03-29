4cbdb760e9e2de7bc554f01b8511b316dbafc27f
Elienay-Alves <elienay8486@gmail.com>
2022-03-29 20:19:00 -0300

    Requisito 4 Feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+const IMG_SRC = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+const IMG_ALT = 'Pikachu crying because the page requested was not found';
+
+describe('Test the NotFound component', () => {
+  it('Have an h2 tag with the text "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+  it('Have an image source', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByAltText(IMG_ALT);
+    expect(img).toHaveAttribute('src', IMG_SRC);
+  });
+});
