17a334afd30a7961324f0ccfe15eb187e5ee7b01
Elienay-Alves <elienay8486@gmail.com>
2022-03-29 19:41:46 -0300

    Requisito 3 Feito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -7,7 +7,7 @@ const PARAGRAPH_1 = (
   'This application simulates a Pokédex, a digital encyclopedia containing all Pokémons');
 const PARAGRAPH_2 = (
   'One can filter Pokémons by type, and see more details for each one of them');
-const IMG_SRCTST = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+const IMG_SRC = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
 
 describe('Test the About Component', () => {
   it('Have Pokédex infos', () => {
@@ -30,6 +30,6 @@ describe('Test the About Component', () => {
   it('Contains an image', () => {
     render(<About />);
     const image = screen.getByRole('img');
-    expect(image.src).toEqual(IMG_SRCTST);
+    expect(image.src).toEqual(IMG_SRC);
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+import data from '../data';
+
+describe('Test the FavoritePokemons component', () => {
+  it('Have the message "No favorite pokemon found"', () => {
+    render(<FavoritePokemons pokemons={ [] } />);
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+  it('Shows all favorites pokemon cards', () => {
+    const cards = [data[0], data[8]];
+    renderWithRouter(<FavoritePokemons pokemons={ cards } />);
+    expect(screen.getByText(/Pikachu/)).toBeInTheDocument();
+    expect(screen.getByText(/Dragonair/)).toBeInTheDocument();
+  });
+});
